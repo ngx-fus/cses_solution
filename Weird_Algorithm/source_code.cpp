ca871c40fd2abe81967b21f8d9f3c80923372d5b
nguyenphu-3204 <nguyenphu3204@outlook.com>
2023-07-03 12:31:14 +0700

    create a branch

---
+++ Weird_Algorithm/source_code.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define cmd(i) cout << (i) << "\n";
+#define even 1
+#define odd 0
+ 
+int main(){
+    int64_t n; 
+    cin>>n;
+    cout<<n<<' ';
+    while(n!=1){
+        if (n%2==0)
+            n/=2;
+        else
+            n=n*3+1;
+        cout<<n<<' '; 
+    }
+ 
+        
+return 0;
+}
